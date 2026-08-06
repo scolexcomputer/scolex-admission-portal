c44fce0967754eda0085c74782927f43d17e9f2a
scolexcomputer <scolexcomputer@gmail.com>
2026-08-06 10:51:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-let slideIndex = 1;
-showSlide(slideIndex);
+let currentSlideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+let slideInterval;
 
-function changeSlide(n) {
-    showSlide(slideIndex += n);
+// Initialize slideshow
+showSlide(currentSlideIndex);
+startAutoSlide();
+
+function showSlide(index) {
+    // Loop around if out of bounds
+    if (index >= slides.length) {
+        currentSlideIndex = 0;
+    } else if (index < 0) {
+        currentSlideIndex = slides.length - 1;
+    } else {
+        currentSlideIndex = index;
+    }
+
+    // Hide all slides
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+
+    // Show current slide
+    slides[currentSlideIndex].style.display = 'block';
 }
 
-function showSlide(n) {
-    let slides = document.getElementsByClassName("slide");
+function changeSlide(step) {
+    // Reset timer on manual click
+    clearInterval(slideInterval);
+    showSlide(currentSlideIndex + step);
+    startAutoSlide();
+}
 
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex - 1].style.display = "block";
+function startAutoSlide() {
+    slideInterval = setInterval(() => {
+        showSlide(currentSlideIndex + 1);
+    }, 4000); // Transitions every 4 seconds
 }
